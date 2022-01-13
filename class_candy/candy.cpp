5480fc2b6fa2b5835e8e6bf0c737480ca6636b49
Matias <74540729+matias1419@users.noreply.github.com>
2022-01-13 13:12:49 +0100

    Delete class_candy directory

--- class_candy/candy.cpp
+++
@@ -1,9 +0,0 @@
-/*
-Cours : INFO F 202
-Projet : Candy Crush
-Auteurs : NIETO NAVARRETE Matias, QUERINJEAN Arnaud
-Date de remise : 13 janvier 2021
-*/
-#include "candy.h"
-// trouver sur https://github.com/BDMR21/candy_crush
-Candy::Candy (const char *filename) : Fl_PNG_Image(filename){}
